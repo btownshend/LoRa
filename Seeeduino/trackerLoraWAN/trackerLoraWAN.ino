bf405bdae42c7cdcdc6bf71879d64efff38f6f33
Brent Townshend <bst@tc.com>
2021-05-01 00:18:20 -0700

    Set battery_status pin LOW before reading voltage

--- Seeeduino/trackerLoraWAN/trackerLoraWAN.ino
+++ Seeeduino/trackerLoraWAN/trackerLoraWAN.ino
@@ -12,11 +12,11 @@ int gpsecho = 20;
 
 unsigned short batteryvoltage() {
   // Not clear what pulling down the status pin is supposed to achieve
-  //pinMode(pin_battery_status , OUTPUT);
-  //digitalWrite(pin_battery_status , LOW);
-  //delay(100);  /// This may cause issues with losing serial chars -- @115,200 100ms = 1152 char
+  pinMode(pin_battery_status , OUTPUT);
+  digitalWrite(pin_battery_status , LOW);
+  delay(1);  /// This may cause issues with losing serial chars -- @115,200 1ms = 12 char
   unsigned short battery = (analogRead(pin_battery_voltage) * 3300 * 11) >> 10;
-  //pinMode(pin_battery_status , INPUT);
+  pinMode(pin_battery_status , INPUT);
 
   SerialUSB.print("Battery: ");
   SerialUSB.println(battery);
@@ -24,6 +24,7 @@ unsigned short batteryvoltage() {
 }
 
 unsigned char batterystatus() {
+  // 0-charging, 1-onbattery
   return digitalRead(pin_battery_status);
 }
 
