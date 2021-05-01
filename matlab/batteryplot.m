afb220d90bd84cd6751fa27b99d948d9e9bb2de6
Brent Townshend <bst@tc.com>
2021-05-01 00:17:51 -0700

    Handle non-string JSON object fields

--- matlab/batteryplot.m
+++ matlab/batteryplot.m
@@ -3,7 +3,11 @@ bat=[];
 for i=1:length(j)
   p=j(i).payload;
   if isfield(p,'object') && isfield(p.object,'battery_status')
-    bat=[bat,struct('deviceName',p.deviceName,'battery_status',p.object.battery_status,'battery_voltage',str2double(p.object.battery_voltage(1:end-1)))];
+    if ischar(p.object.battery_voltage)
+      bat=[bat,struct('deviceName',p.deviceName,'battery_status',p.object.battery_status,'battery_voltage',str2double(p.object.battery_voltage(1:end-1)))];
+    else
+      bat=[bat,struct('deviceName',p.deviceName,'battery_status',p.object.battery_status,'battery_voltage',p.object.battery_voltage)];
+    end
   elseif isfield(p,'batteryLevel')
     bat=[bat,struct('deviceName',p.deviceName,'battery_status',p.batteryLevelUnavailable,'battery_voltage',p.batteryLevel)];
   end
@@ -11,7 +15,7 @@ end
 setfig('battery');clf;
 udev=unique({bat.deviceName});
 for i=1:length(udev)
-  sel=strcmp(udev{i},{bat.deviceName}) & [bat.battery_voltage]>1;
+  sel=strcmp(udev{i},{bat.deviceName}) & [bat.battery_voltage]>0;
   plot(find(sel),[bat(sel).battery_voltage]);
   hold on;
 end
--- matlab/locplot.m
+++ matlab/locplot.m
@@ -3,7 +3,11 @@ loc=[];
 for i=1:length(j)
   p=j(i).payload;
   if isfield(p,'object') && isfield(p.object,'latitude')
-    loc=[loc,struct('deviceName',p.deviceName,'latitude',str2double(p.object.latitude(1:end-2)),'longitude',str2double(p.object.longitude(1:end-2)))];
+    if ischar(p.object.latitude)
+      loc=[loc,struct('deviceName',p.deviceName,'latitude',str2double(p.object.latitude(1:end-2)),'longitude',str2double(p.object.longitude(1:end-2)))];
+    else
+      loc=[loc,struct('deviceName',p.deviceName,'latitude',p.object.latitude,'longitude',p.object.longitude)];
+    end
   end
 end
 bad=[loc.latitude]<37 | [loc.longitude]>-122;
