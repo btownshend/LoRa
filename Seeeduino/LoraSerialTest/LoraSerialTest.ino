388f15ad2d36392dd7677c6d73efe8c9847dafbd
Brent Townshend <bst@tc.com>
2021-05-08 12:33:32 -0700

    Use Scheduler for LoraSerialTest

--- Seeeduino/LoraSerialTest/LoraSerialTest.ino
+++ Seeeduino/LoraSerialTest/LoraSerialTest.ino
@@ -1,23 +1,30 @@
+#include <Scheduler.h>
+
 void setup()
 {
-    Serial1.begin(115200);
-    SerialUSB.begin(115200);
-    for (int i=0;i<5;i++) {
-      SerialUSB.print(i,DEC);
-      SerialUSB.print("...");
-      delay(1000);
-    }
-    SerialUSB.println("LoraSerialTest Ready");
+  Serial1.begin(115200);   // Assuming baud rate already changed, default is 9600 for new boards
+  SerialUSB.begin(115200);
+  for (int i = 0; i < 5; i++) {
+    SerialUSB.print(i, DEC);
+    SerialUSB.print("...");
+    delay(1000);
+  }
+  SerialUSB.println("LoraSerialTest Ready");
+  Scheduler.startLoop(loop2);
 }
- 
-void loop()
-{
-    while(Serial1.available())
-    {
-        SerialUSB.write(Serial1.read());
-    }
-    while(SerialUSB.available())
-    {
-        Serial1.write(SerialUSB.read());
-    }
+
+void loop() {
+  while (Serial1.available())
+  {
+    SerialUSB.write(Serial1.read());
+  }
+  yield();
+}
+
+void loop2() {
+  while (SerialUSB.available())
+  {
+    Serial1.write(SerialUSB.read());
+  }
+  yield();
 }
