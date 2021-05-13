8147569038cf4e583b184d3529ecd6987467ca55
Brent Townshend <bst@tc.com>
2021-05-13 16:41:47 -0700

    Added inktest.ino, sercomtest.ino

---
+++ Seeeduino/sercomtest/sercomtest.ino
@@ -0,0 +1,32 @@
+#include "wiring_private.h" // pinPeripheral() function
+
+#define PIN_SERIAL3_RX       (5ul)   // PA15 SERCOM2:pad3  (D5 on connector)
+#define PIN_SERIAL3_TX       (4ul)   // PA14 SERCOM2:pad2  (D4 on connector)
+Uart Serial3( &sercom2, PIN_SERIAL3_RX, PIN_SERIAL3_TX, SERCOM_RX_PAD_3, UART_TX_PAD_2 ) ;
+
+void SERCOM2_Handler() {
+  Serial3.IrqHandler();
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+  Serial.begin(115200);
+  //Serial3.begin(230400);  // For e-ink display
+  Serial3.begin(9600);  // For GPS
+  // Change PA14,PA15 to SERCOM (mode C)
+  pinPeripheral(PIN_SERIAL3_RX, PIO_SERCOM);
+  pinPeripheral(PIN_SERIAL3_TX, PIO_SERCOM);
+}
+
+void loop() {
+  while (Serial3.available())
+  {
+    Serial.write(Serial3.read());
+  }
+  while (Serial.available())
+  {
+    Serial3.write(Serial.read());
+  }
+}
