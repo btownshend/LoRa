a7ce39ce9a922d0bc4a712b5d8d630559016525a
Brent Townshend <bst@tc.com>
2021-05-15 15:36:50 -0700

    Create savedata.sh

---
+++ mqtt/savedata.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+dt=$(date "+%Y%m%d-%H%M%S")
+logfile=MQTT+${dt}.log
+echo Saving application topic in $logfile
+/usr/local/homebrew/bin/mosquitto_sub -h 192.168.0.246 -t "application/#" -F %J > $logfile
+
+
