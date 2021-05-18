2c53f308483236475fe95f2d0c073f96e7a46f2a
Brent Townshend <bst@tc.com>
2021-05-17 19:02:26 -0700

    Update sercomtest.ino

--- Seeeduino/sercomtest/sercomtest.ino
+++ Seeeduino/sercomtest/sercomtest.ino
@@ -10,11 +10,14 @@ void SERCOM2_Handler() {
 
 void setup() {
   Serial.begin(115200);
+  delay(100);
   //Serial3.begin(230400);  // For e-ink display
   Serial3.begin(9600);  // For GPS or BLE
+  //Serial3.begin(115200);  // For LoraWan (after baud adjusted)
   // Change PA14,PA15 to SERCOM (mode C)
   pinPeripheral(PIN_SERIAL3_RX, PIO_SERCOM);
   pinPeripheral(PIN_SERIAL3_TX, PIO_SERCOM);
+  Serial.println("sercomtest ready");
 }
 
 void loop() {
