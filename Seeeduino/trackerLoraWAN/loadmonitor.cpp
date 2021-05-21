68ca230240b0ddf5c96c5f992699ad64c55d1468
Brent Townshend <bst@tc.com>
2021-05-21 15:24:23 -0700

    Format change

--- Seeeduino/trackerLoraWAN/loadmonitor.cpp
+++ Seeeduino/trackerLoraWAN/loadmonitor.cpp
@@ -18,7 +18,7 @@ void loadmonitorloop(void) {
     samples++;
     if (now-avgstart > 5000000) {
 	float load = (now-avgstart-mintime*samples)*100/(now-avgstart);
-	notice("Loop: min=%d,max=%d,avg=%ld usec -> %f%%\n", mintime, maxtime, (now-avgstart)/samples,load);
+	notice("Loop: min=%d,max=%d,avg=%ld usec -> %.0f%%\n", mintime, maxtime, (now-avgstart)/samples,load);
 	samples=0;
 	avgstart=now;
 	mintime=99999;
