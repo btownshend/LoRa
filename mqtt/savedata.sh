694da2d05c7000cd7a6c41ccb221e19af6fec196
Brent Townshend <bst@tc.com>
2021-07-01 15:50:13 -0700

    Use .189 for base station

--- mqtt/savedata.sh
+++ mqtt/savedata.sh
@@ -2,6 +2,6 @@
 dt=$(date "+%Y%m%d-%H%M%S")
 logfile=MQTT+${dt}.log
 echo Saving application topic in $logfile
-/usr/local/homebrew/bin/mosquitto_sub -h 192.168.0.246 -t "application/#" -F %J > $logfile
+/usr/local/homebrew/bin/mosquitto_sub -h 192.168.0.189 -t "application/#" -F %J > $logfile
 
 
