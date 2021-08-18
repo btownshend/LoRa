580d2eb2778f93d65499ecd5bf696d9020d4b1e8
Brent Townshend <bst@tc.com>
2021-08-18 15:20:24 -0700

    Update ardtest.m

--- matlab/magcal/ardtest.m
+++ matlab/magcal/ardtest.m
@@ -1,9 +1,10 @@
 if ~exist('s','var')
   serialportlist();
-  devname='/dev/cu.usbmodem14601';
+  devname='/dev/cu.usbmodem401';
   s=ardserial(devname);
 end
 s.writeline('x');   % To cancel any prior
+pause(1);
 s.flush();
 s.writeline('LAT+VER');
 if (~s.waitfor('<LORA: +VER',1))
