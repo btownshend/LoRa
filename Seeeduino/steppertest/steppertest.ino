9b55db8a37ef50b373a7f61b4a0e51b11092840e
Brent Townshend <bst@tc.com>
2021-08-17 22:07:51 -0700

    Update steppertest.ino

--- Seeeduino/steppertest/steppertest.ino
+++ Seeeduino/steppertest/steppertest.ino
@@ -15,8 +15,17 @@ const int SENSORPIN = A0;
 #define MOTOR_1B 10
 #define MOTOR_2A 11
 #define MOTOR_2B 5
-AccelStepper stepper(AccelStepper::FULL4WIRE, MOTOR_1A,MOTOR_1B,MOTOR_2B,MOTOR_2A);
-const int SENSORPIN = 17;
+// 1A,1B,2B,2A - works for old unit, not for new
+// 1A,1B,2A,2B - erratic, jumps by up to 90 deg
+// 1A,2A,1B,2B - vibrates
+// 1A,2A,2B,1B - vibrates
+// 1A,2B,1B,2A - vibrates
+// 1A,2B,2A,1B - vibrates
+//AccelStepper stepper(AccelStepper::FULL4WIRE, MOTOR_1A,MOTOR_1B,MOTOR_2B,MOTOR_2A);
+AccelStepper stepper(AccelStepper::FULL4WIRE, MOTOR_2A,MOTOR_2B,MOTOR_1B,MOTOR_1A);
+#define OS_OUTPUT 17 // analog read
+#define OS_EN_L  18 // enable LED emitter when low
+#define SENSORPIN OS_OUTPUT
 #endif
 
 #define USEINTERRUPTS
@@ -37,6 +46,7 @@ void TimerHandler0(void)
 }
 
 #define TIMER0_INTERVAL_US     100
+// #define TIMER0_INTERVAL_US     1000000
 void setuptimer()
 {
   // Interval in microsecs
@@ -54,16 +64,19 @@ void setup()
     delay(2000);
     SerialUSB.println("steppertest");
     
-    const int maxspeed=1200; // X27.168 spec says maximum speed is 600 deg/s -> 1200 step/s
-    const int maxaccel=12000;    // maxspeed/maxaccel gives time to reach full speed
+    const float maxspeed=1200; // X27.168 spec says maximum speed is 600 deg/s -> 1200 step/s
+    const float maxaccel=12000;    // maxspeed/maxaccel gives time to reach full speed
     stepper.setMaxSpeed(maxspeed);  
     stepper.setAcceleration(maxaccel);   // Acceleration tuned for the right "look" (4000 step/s/s will get it up to vmax after rotating 90 deg, but seems to lose steps then)
     float tmax=maxspeed*1.0/maxaccel;
     float degmax=maxaccel*tmax*tmax/2/2;
-    sprintf(fmtbuf,"Stepper setup to reach max speed of %d deg/sec after %.2f sec, %.0f degrees of rotation", maxspeed/2, tmax, degmax);
+    sprintf(fmtbuf,"Stepper setup to reach max speed of %f deg/sec after %.2f sec, %.0f degrees of rotation", maxspeed/2, tmax, degmax);
     SerialUSB.println(fmtbuf);
 
     pinMode(SENSORPIN, INPUT);
+#ifdef PROTOBOARD
+    pinMode(OS_EN_L, OUTPUT);
+#endif
 #ifdef USEINTERRUPTS
     setuptimer();
 #endif
@@ -81,7 +94,26 @@ void gotoangle(int angle) {
 
 // Get current reading from position sensor
 int getsensor() {
+#ifdef SEEEDUINOBOARD
     return analogRead(SENSORPIN);
+#elif defined(PROTOBOARD)
+    pinMode(OS_EN_L, OUTPUT);
+    digitalWrite(OS_EN_L, LOW);
+    delayMicroseconds(1);
+    int os1 = analogRead(OS_OUTPUT);  // With LED enabled
+    digitalWrite(OS_EN_L, HIGH);
+    delayMicroseconds(1);
+    int os2 = analogRead(OS_OUTPUT); // LED off
+    pinMode(OS_EN_L,INPUT);
+    int val=os2-os1;
+    static float background=15;
+    if (val<30 ) {  // Background is typical ~15
+	background=background*0.999+val*0.001;
+    }
+    return val-int(background+0.5);
+#else
+#error BOARD not defined
+#endif
 }
 
 void test1()
@@ -102,6 +134,8 @@ void test1()
 	gotoangle(angle);
     } else if (togo!= lasttogo) {
 	Serial.print(stepper.distanceToGo());
+	Serial.print("@");
+	Serial.print(stepper.speed());
 	Serial.print(",");
 	lasttogo=togo;
     }
@@ -112,7 +146,7 @@ void test2() {
 
     int togo=stepper.distanceToGo();
     if (togo == 0) {
-	angle+=107;
+	angle+=90;
 	delay(1000);
 	gotoangle(angle);
     }
@@ -120,7 +154,7 @@ void test2() {
     int pos = stepper.currentPosition()%STEPSPERREV;
     static int lasts=0;
     static int lastpos=0;
-    if (s!=lasts || pos!=lastpos) {
+    if (s>10 || abs(pos-lastpos)>5) {
 	Serial.print(millis());
 	Serial.print(",");
 	Serial.print(pos);
@@ -202,8 +236,32 @@ void test4() {
     }
 }
 
+void test5() {
+    while (true) {
+	const int period=100;
+	digitalWrite(MOTOR_1A,HIGH);
+	SerialUSB.print("1A ");
+	delay(period);
+	digitalWrite(MOTOR_1A,LOW);
+	digitalWrite(MOTOR_2A,HIGH);
+	SerialUSB.print("2A ");
+	delay(period);
+	digitalWrite(MOTOR_2A,LOW);
+	digitalWrite(MOTOR_1B,HIGH);
+	SerialUSB.print("1B ");
+	delay(period);
+	digitalWrite(MOTOR_1B,LOW);
+	digitalWrite(MOTOR_2B,HIGH);
+	SerialUSB.print("2B ");
+	delay(period);
+	digitalWrite(MOTOR_2B,LOW);
+	SerialUSB.println("OFF");
+	//delay(period);
+    }
+}
+
 void loop() {
-    const int runtest = 4;
+    const int runtest = 2;
     if (runtest==1)
 	test1();
     else if (runtest==2)
@@ -212,9 +270,11 @@ void loop() {
 	test3();
     else if (runtest==4)
 	test4();
+    else if (runtest==5)
+	test5();
     else
 	SerialUSB.println("Bad test");
-    delay(7);
+    delay(1);
 #ifndef USEINTERRUPTS
     stepper.run();
 #endif
