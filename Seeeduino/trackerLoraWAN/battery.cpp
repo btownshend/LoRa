7d8ae672c6b94f39624d2e9951c0424b9a8f40e5
Brent Townshend <bst@tc.com>
2021-08-17 22:08:03 -0700

    Update battery.cpp

--- Seeeduino/trackerLoraWAN/battery.cpp
+++ Seeeduino/trackerLoraWAN/battery.cpp
@@ -23,19 +23,17 @@ unsigned char batterystatus() {
 void batterysetup() {
       pinMode(pin_battery_status, INPUT);
 }
-#endif
-
-#ifdef PROTOBOARD
+#elif defined(PROTOBOARD)
 static unsigned short battery=0;
 
 unsigned short batteryvoltage() {
   // Enable the battery measurement
-  digitalWrite(PIN_BATT_MEAS_EN , LOW);
-  delay(1);  /// This may cause issues with losing serial chars -- @115,200 1ms = 12 char (should be OK when using Scheduler)
-  battery = (analogRead(PIN_BATT_MEAS) * 6600) >> 10;
   digitalWrite(PIN_BATT_MEAS_EN , HIGH);
-
-  notice("Battery: %d\n",battery);
+  delay(1);  /// This may cause issues with losing serial chars -- @115,200 1ms = 12 char (should be OK when using Scheduler)
+  int batRead=analogRead(PIN_BATT_MEAS);
+  //digitalWrite(PIN_BATT_MEAS_EN , LOW);
+  battery = (batRead * 6600) >> 10;
+  notice("Battery: analog: %d, mV: %d\n",batRead, battery);
   return battery;
 }
 
@@ -47,6 +45,8 @@ unsigned char batterystatus() {
 
 void batterysetup() {
       pinMode(PIN_BATT_MEAS_EN, OUTPUT);
-      digitalWrite(PIN_BATT_MEAS_EN,HIGH);
+      digitalWrite(PIN_BATT_MEAS_EN,LOW);
 }
+#else
+#error Board not defined
 #endif
