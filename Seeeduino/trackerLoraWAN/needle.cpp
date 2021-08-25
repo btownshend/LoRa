f2395dc7ba0fd04e3c605ee9b577476c3e465c64
Brent Townshend <bst@tc.com>
2021-08-24 23:56:30 -0700

    Verify nfound

--- Seeeduino/trackerLoraWAN/needle.cpp
+++ Seeeduino/trackerLoraWAN/needle.cpp
@@ -193,7 +193,16 @@ void Needle::sensorcheck(void) {
     if (spinning>0 && stepper.distanceToGo()<20)
 	move(20);
     
-    if (nfound==360) {
+    if (nfound>=360) {
+	if  (nfound>360) {
+	    int check=0;
+	    for (int i=0;i<360;i++)
+		if (sensorVals[i]>=0)
+		    check++;
+	    SerialUSB.print("check=");
+	    SerialUSB.print(check);
+	    SerialUSB.print(", ");
+	}
 	if (spinning>0)
 	    spinning--;
 	unsigned long tic=millis();
