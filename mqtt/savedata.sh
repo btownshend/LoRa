96f58ad0cf774229c560864cd8c9bbb284cd1bf7
Brent Townshend <bst@tc.com>
2021-08-25 00:52:24 -0700

    Use rotatelogs for savedata

--- mqtt/savedata.sh
+++ mqtt/savedata.sh
@@ -1,7 +1,15 @@
 #/bin/bash
+PATH=${PATH}:/usr/local/homebrew/bin
 dt=$(date "+%Y%m%d-%H%M%S")
 logfile=MQTT+${dt}.log
 echo Saving application topic in $logfile
-/usr/local/homebrew/bin/mosquitto_sub -h 192.168.0.189 -t "application/#" -F %J > $logfile
-
+if [ $(uname) == "Linux" ]
+then
+  host=127.0.0.1
+else
+  host=192.168.0.189
+fi
+echo Listening to $host
+# mosquitto_sub -h $host -t "application/#" -F %J > $logfile
+mosquitto_sub -h $host -t "application/#" -F %J | rotatelogs -lf logs/MQTT-%Y%m%d-%X.log 3600
 
